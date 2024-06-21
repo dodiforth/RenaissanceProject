1449ae764d43b0d923d525ec28c64cab0efaf370
Dowon Kim <dowoniscool@yahoo.com>
2024-06-21 20:55:37 +0200

    chore : Create basic Tab Bar with items accordinly

--- RenaissanceProject/Controllers/Core/RPTabBarController.swift
+++ RenaissanceProject/Controllers/Core/RPTabBarController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class RPTabBarController: UITabBarController {
+final class RPTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUpTabs()
     }
 
-
+    private func setUpTabs() {
+        let characterVC = RPCharacterViewController()
+        let locationVC = RPLocationViewController()
+        let episodeVC = RPEpisodeViewController()
+        let settingsVC = RPSettingsViewController()
+        
+        characterVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodeVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: characterVC)
+        let nav2 = UINavigationController(rootViewController: locationVC)
+        let nav3 = UINavigationController(rootViewController: episodeVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Location", image: UIImage(systemName: "location"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "film"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gearshape"), tag: 4)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
 }
 
