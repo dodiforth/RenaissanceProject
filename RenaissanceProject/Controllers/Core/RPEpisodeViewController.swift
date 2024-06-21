3b1060af5e48d9b34076d77714aa5bea286c8a4b
Dowon Kim <dowoniscool@yahoo.com>
2024-06-21 20:21:35 +0200

    Added new controller files for Tab bar in Core

---
+++ RenaissanceProject/Controllers/Core/RPEpisodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RPEpisodeViewController.swift
+//  RenaissanceProject
+//
+//  Created by Dowon Kim on 21/06/2024.
+//
+
+import UIKit
+
+class RPEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RenaissanceProject/Models/RPCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RPCharacter.swift
+//  RenaissanceProject
+//
+//  Created by Dowon Kim on 21/06/2024.
+//
+
+import Foundation
