73809c7dfa7134b151b1bcf0d365ab1363dc719e
Dowon Kim <dowoniscool@yahoo.com>
2024-06-22 22:16:35 +0200

    Add Spinner Animation to Character View

--- RenaissanceProject/Views/CharacterListView.swift
+++ RenaissanceProject/Views/CharacterListView.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 final class CharacterListView: UIView {
-
-   override init(frame: CGRect) {
+    
+    private let viewModel = CharacterListViewVM()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
         super.init(frame: frame)
-       translatesAutoresizingMaskIntoConstraints = false
+        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemBlue
+        
+        addSubview(spinner)
+        addConstraints()
+        spinner.startAnimating()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
 }
